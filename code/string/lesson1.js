f9fafa54152ce63f5ccce832c4714156c941890d
zzy <1430971200@qq.com>
2019-04-04 11:17:27 +0800

    字符反转优化1

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,7 +1,14 @@
+// export default (str) => {
+//   let arr = str.split(' ')
+//   let result = arr.map(item => {
+//     return item.split('').reverse().join('')
+//   })
+//   return result.join(' ')
+// }
 export default (str) => {
-  let arr = str.split(' ')
-  let result = arr.map(item => {
-    return item.split('').reverse()
-  })
-  return result
+  // 1.字符串按空格进行分隔，保存数组
+  // 2.对数组进行遍历，进行反序操作，
+  return str.split(' ').map(item => {
+    return item.split('').reverse().join('')
+  }).join(' ')
 }
